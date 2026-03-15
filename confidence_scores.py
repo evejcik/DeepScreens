48ed73fe93f48d67c50812fa0f29f37148da9015
Emma Vejcik <evejcik@gmail.com>
2026-03-15 17:23:17 -0400

    edits

--- confidence_scores.py
+++ confidence_scores.py
@@ -6,11 +6,23 @@ import numpy as np
 import argparse
 import pandas as pd
 
+# create vector of x,y confidence vectors, generated from tabular data from hand-annotated data
+# pass this vector to optical flow calculation
+
+# Add a variance head to your 2‑D model (predict a per‑joint log‑σ²) and train it with a negative‑log‑likelihood loss. 
+# The variance then serves the same purpose as the heat‑map’s spread.
+
+
+
 def confidence_vec(data_path):
     confidenceX = data_path.loc['x']
     confidenceY = data_path.loc['y']
 
     c_i = list(zip(confidenceX, confidenceY))
+
+    for x,y in c_i:
+        scaled_x = (x - c_i[x].min()) / (c_i[x].max() - c_i[x].min())
+        scaled_y = (y - c_i[y].min()) / (c_i[y].max() - c_i[y].min())
     return c_i
 
 
