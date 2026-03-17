9132d85cf6a3a9971d882ad1e13d94818fdb5eca
Emma Vejcik <evejcik@gmail.com>
2026-03-17 12:58:15 -0400

    beta

---
+++ Functional Stuff/Beta Stuff/beta_diganostic.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+import argparse
+
+## MAP with weak Beta prior
+
+## Logit Gaussian
+
+## Jitter and clip
+
+## Hierarchical / Empirical‑Bayes pooling
+
+## Isotonic regression (non‑parametric)
+
+## Simple Binning
