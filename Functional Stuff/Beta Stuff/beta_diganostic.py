d17a5d69c8fd8106f3997d23ea6c97f87f1636df
Emma Vejcik <evejcik@gmail.com>
2026-03-17 23:48:09 -0400

    neg log posterior because i can

--- Functional Stuff/Beta Stuff/beta_diganostic.py
+++ Functional Stuff/Beta Stuff/beta_diganostic.py
@@ -1,13 +1,30 @@
 import pandas as pd
 import numpy as np
 import argparse
+import scipy.special as sc
 
+
+def beta_func(alpha, beta):
+    return sc.beta(alpha, beta)
 
 ##negative log posterior J(alpha, beta)
-def neg_log_posterior(alpha, beta, scores):
+def neg_log_posterior(alpha, beta, scores, alpha_prior, beta_prior):
     c = scores
     c = np.clip(c, float(1e-6), 1.0 - (1e-6) )
     N = len(scores)
+
+    #(alpha - 1)
+    alpha_1 = (alpha - 1.0)
+    #sigma log(c_i) 
+    sigma_log_ci = np.sum(np.log(c))
+    #beta -1
+    beta_1 = (beta - 1.0)
+
+    sigma_log_1_ci = np.sum(np.log(1.0 - c))
+    n_log_beta = N * np.log(beta_func(alpha, beta))
+
+    sum_term = alpha_1 * sigma_log_ci + beta_1 * sigma_log_1_ci - n_log_beta
+    final = sum_term + (alpha - alpha_prior) + (beta - beta_prior)
 
 
 ## MAP with weak Beta prior
