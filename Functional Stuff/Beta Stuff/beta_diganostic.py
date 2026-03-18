f72b0e1f310bf4621ee70a3618d1464bfc36dbfd
Emma Vejcik <evejcik@gmail.com>
2026-03-18 00:02:40 -0400

    neg log posterior

--- Functional Stuff/Beta Stuff/beta_diganostic.py
+++ Functional Stuff/Beta Stuff/beta_diganostic.py
@@ -5,7 +5,12 @@ from scipy.special import betaln   # log of the beta function B(α,β)
 
 
 ##negative log posterior J(alpha, beta)
-def neg_log_posterior(alpha, beta, scores, eps: float = 1e-6, alpha_prior, beta_prior):
+def neg_log_posterior(alpha, beta, scores, eps: float = 1e-6, alpha_prior = 2.0, beta_prior = 2.0): 
+    ##FOR OPTIMIZATION'S SAKE: consider placing a different distribution over alpha and beta parameters 
+    # i.e., a gamma or hierarchical bayes distrbution
+    #as opposed to the initial exponential distribution
+
+
     c = scores
     c = np.clip(c, eps, 1.0 - eps)
     N = len(scores)
@@ -20,8 +25,12 @@ def neg_log_posterior(alpha, beta, scores, eps: float = 1e-6, alpha_prior, beta_
     sigma_log_1_ci = np.sum(np.log(1.0 - c))
     n_log_beta = N * np.log(betaln(alpha, beta))
 
-    sum_term = alpha_1 * sigma_log_ci + beta_1 * sigma_log_1_ci - n_log_beta
-    final = sum_term + (alpha - alpha_prior) + (beta - beta_prior)
+    negative_log_likelihood = alpha_1 * sigma_log_ci + beta_1 * sigma_log_1_ci - n_log_beta
+
+    log_prior = - (alpha - alpha_prior) - (beta - beta_prior) #right now, set to 2 and 2 to keep prior weak
+    final = negative_log_likelihood - log_prior
+
+    return float(final)
 
 
 ## MAP with weak Beta prior
