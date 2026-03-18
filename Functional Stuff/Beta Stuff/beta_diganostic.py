8d114f25cfe7e57184af37101d9a3ec04cebcb66
Emma Vejcik <evejcik@gmail.com>
2026-03-17 23:38:18 -0400

    catchup

--- Functional Stuff/Beta Stuff/beta_diganostic.py
+++ Functional Stuff/Beta Stuff/beta_diganostic.py
@@ -2,6 +2,14 @@ import pandas as pd
 import numpy as np
 import argparse
 
+
+##negative log posterior J(alpha, beta)
+def neg_log_posterior(alpha, beta, scores):
+    c = scores
+    c = np.clip(c, float(1e-6), 1.0 - (1e-6) )
+    N = len(scores)
+
+
 ## MAP with weak Beta prior
 
 ## Logit Gaussian
