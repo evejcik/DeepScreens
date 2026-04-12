a660644db080d6268af1d26a94d15c1ca2955911
Emma Vejcik <evejcik@gmail.com>
2026-04-12 19:47:42 -0400

    Kalman

--- Interpolation/2D Interpolation/2D_interpolation.py
+++ Interpolation/2D Interpolation/2D_interpolation.py
@@ -10,6 +10,8 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from scipy.signal import savgol_filter
 
+from kalman import KalmanFilter, apply_kalman_to_group, apply_kalman_filter
+
 
 #need to get long data csv
 #need to get json
@@ -26,15 +28,18 @@ def sav_golay(df, k, p):
     #we need to get the coordinates for (x,y) for each film, instance_id, frame_id, joint_id
     # def savgol_filter_mine(df, k, p):
 
-
-    df['x_smooth'] = savgol_filter(df['x'], window_length = k, polyorder = p)
-    df['y_smooth'] = savgol_filter(df['x'], window_length = k, polyorder = p)
+    #only apply to trusted points -> to smooth between joints for aesthetics essentially.
+    x = df[df['reliability_category_int'] == 0]['x']
+    y = df[df['reliability_category_int'] == 0]['y']
+    df['x_smooth'] = savgol_filter(x, window_length = k, polyorder = p)
+    df['y_smooth'] = savgol_filter(y, window_length = k, polyorder = p)
     return df
 
 
 def main(csv):
     df = pd.read(csv)
     df = sav_golay(df, k = 7, p = 10) #idk, guessing at these, finetune later
+    df = apply_kalman_filter(df)
 
 if __name__ == "__main__":
     ap = argparse.ArgumentParser()
